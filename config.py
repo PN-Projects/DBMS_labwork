b6ed960ec968850ea182db9c9e3cc145cf33f6b4
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2024-12-18 16:05:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,4 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-POSTGRESQL_DATABASE_URI = os.getenv("POSTGRESQL_DATABASE_URI", "")
-
-SQLITE_DATABASE_FILE = os.getenv("SQLITE_DATABASE_FILE", "local_salespeople_db.sqlite")
-
-MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
-MYSQL_USER = os.getenv("MYSQL_USER", "root")
-MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
-MYSQL_DATABASE = os.getenv("MYSQL_DATABASE", "")
+DATABASE_URI = os.getenv("DATABASE_URI", "")
